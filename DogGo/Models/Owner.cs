90a46a44b87faea1afe7015d554f94288b7210cb
Austin Ring <aring97@gmail.com>
2020-12-11 15:29:56 -0600

    finished chapter 2 exercise

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -11,6 +11,7 @@ namespace DogGo.Models
         public string Email {get; set;}
         public string Name { get; set; }
         public string Address { get; set; }
+        public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
         public string Phone { get; set; }
     }
--- DogGo/Repositories/IDogRepository.cs
+++ DogGo/Repositories/IDogRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using DogGo.Models;
 
 namespace DogGo.Repositories
 {
     public interface IDogRepository
     {
-
+        List<Dog> GetAllDogs();
+        Dog GetDogById(int id);
+        void AddDog(Dog dog);
+        void UpdateDog(Dog dog);
+        void DeleteDog(int dogId);
     }
 }
